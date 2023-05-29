dd1d9aff9a37fac90aee37014c849aa0257a8048
jakergrossman <jake.r.grossman@gmail.com>
2023-05-29 00:27:41 -0500

    chore(2022): Rewrite day4 to use a range trait

--- src/aoc2022/day04.rs
+++ src/aoc2022/day04.rs
@@ -1,37 +1,54 @@
+use std::ops::RangeInclusive;
+
 use crate::{aoclib::day::*, run_day};
 
+trait InclusiveRangeExt {
+    fn contains_range(&self, other: &Self) -> bool;
+
+    fn contains_range_reflective(&self, other: &Self) -> bool {
+        self.contains_range(other) || other.contains_range(self)
+    }
+
+    fn overlaps(&self, other: &Self) -> bool;
+
+    fn overlaps_reflective(&self, other: &Self) -> bool {
+        self.overlaps(other) || other.overlaps(self)
+    }
+}
+
+impl<T> InclusiveRangeExt for RangeInclusive<T>
+where
+    T: PartialOrd
+{
+    fn contains_range(&self, other: &Self) -> bool {
+        self.contains(other.start()) && self.contains(other.end())
+    }
+
+    fn overlaps(&self, other: &Self) -> bool {
+        self.contains(other.start()) || self.contains(other.end())
+    }
+}
+
 struct ElfPair {
-    elf1: (u128, u128),
-    elf2: (u128, u128),
+    elf1: RangeInclusive<u128>,
+    elf2: RangeInclusive<u128>
 }
 
 impl ElfPair {
-    fn new(line: &str) -> ElfPair {
-        let pairs: Vec<_> 
-            = line.split(",").map(|range| range.split("-").collect::<Vec<_>>()).collect();
+    fn new(line: &str) -> Option<ElfPair> {
+        let (elf1_str, elf2_str) = line.split_once(",")?;
+        let (elf1_start, elf1_stop) = elf1_str.split_once("-")?;
+        let (elf2_start, elf2_stop) = elf2_str.split_once("-")?;
 
-        ElfPair {
-            elf1: 
-                (u128::from_str_radix(pairs[0][0], 10).unwrap(), 
-                 u128::from_str_radix(pairs[0][1], 10).unwrap()),
-            elf2:
-                (u128::from_str_radix(pairs[1][0], 10).unwrap(), 
-                 u128::from_str_radix(pairs[1][1], 10).unwrap()),
-        }
-    }
+        let elf1 = elf1_start.parse().ok()?..=elf1_stop.parse().ok()?;
+        let elf2 = elf2_start.parse().ok()?..=elf2_stop.parse().ok()?;
 
-    fn is_fully_overlapping(&self) -> bool {
-        ((self.elf1.0 <= self.elf2.0) && (self.elf1.1 >= self.elf2.1)) ||
-        ((self.elf2.0 <= self.elf1.0) && (self.elf2.1 >= self.elf1.1))
-    }
-
-    fn is_overlapping(&self) -> bool {
-        (self.elf1.0 <= self.elf2.1) && (self.elf2.0 <= self.elf1.1)
+        Some(ElfPair { elf1, elf2 })
     }
 }
 
 fn serialize(s: String) -> Vec<ElfPair> {
-    s.lines().map(ElfPair::new).collect()
+    s.lines().map(|l| ElfPair::new(l).expect("Invalid input")).collect()
 }
 
 pub fn run(is_sample: bool) {
@@ -39,9 +56,9 @@ pub fn run(is_sample: bool) {
 }
 
 fn part1(pairs: Vec<ElfPair>) -> u128 {
-    pairs.iter().filter(|pair| pair.is_fully_overlapping()).count() as u128
+    pairs.iter().filter(|pair| pair.elf1.contains_range_reflective(&pair.elf2)).count() as u128
 }
 
 fn part2(pairs: Vec<ElfPair>) -> u128 {
-    pairs.iter().filter(|pair| pair.is_overlapping()).count() as u128
+    pairs.iter().filter(|pair| pair.elf1.overlaps_reflective(&pair.elf2)).count() as u128
 }
